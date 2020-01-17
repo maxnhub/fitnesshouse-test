d17708a7fd9ad4b8e1ca7e33d5d9a394b1d788dc
maxnhub <maxnikulnikov@gmail.com>
2020-01-17 16:45:15 +0300

    Initial build subtree commit

---
+++ build/precache-manifest.21a7fc0aefccf3dea8ffe1b8b1124697.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c33888115da8d35d3c9888296fe518",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bc73172b6cd00231618",
+    "url": "/static/css/main.6f205877.chunk.css"
+  },
+  {
+    "revision": "76f55d02cc7651db7121",
+    "url": "/static/js/2.c7cf5486.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c7cf5486.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bc73172b6cd00231618",
+    "url": "/static/js/main.401dc61f.chunk.js"
+  },
+  {
+    "revision": "152e10f804d3f5dc4973",
+    "url": "/static/js/runtime-main.f54b1d43.js"
+  },
+  {
+    "revision": "e1db67de818d501f21e3f7f84414583d",
+    "url": "/static/media/super1.e1db67de.png"
+  }
+]);
